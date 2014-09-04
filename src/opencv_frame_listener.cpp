f121df06bda8a956d1d047479bf83ddaa8750b76
Julius Kammerl <julius@kammerl.de>
2014-09-04 01:14:05 -0700

    minor

--- src/opencv_frame_listener.cpp
+++ src/opencv_frame_listener.cpp
@@ -7,7 +7,7 @@
 #include "opencv_frame_listener.h"
 
 OpenCVFrameListener::OpenCVFrameListener() {
-  cv::namedWindow( "Depth video", cv::WINDOW_AUTOSIZE );
+  cv::namedWindow( "Depth Video", cv::WINDOW_AUTOSIZE );
 }
 
 OpenCVFrameListener::~OpenCVFrameListener() {
@@ -17,7 +17,6 @@ void OpenCVFrameListener::onNewCVFrame(const cv::Mat& cv_frame) {
   cv::Mat frame_normalized;
   cv::normalize(cv_frame, frame_normalized, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 
-  std::cout<<"Frame received\n" << cv_frame.cols << "x"<<cv_frame.rows<<std::endl;
-  cv::imshow( "Depth video", frame_normalized );
+  cv::imshow( "Depth Video", frame_normalized );
   cv::waitKey(1);
 }
